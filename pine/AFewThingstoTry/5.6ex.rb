862b60f62df5205387d8ec4efbdbf82da8ee1de6
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-29 11:28:03 -0500

    Bigger and better number ex. I get a Fixnum into String error here..

--- pine/AFewThingstoTry/5.6ex.rb
+++ pine/AFewThingstoTry/5.6ex.rb
@@ -1,6 +1,7 @@
 # A Few Things to Try...
 # 5.6-Exercise page 28
 
+# ---- Full name greeting ----
 # Ask for full name, and greet user
 # How I did it
 puts 'Hello. What is your first name?'
@@ -30,3 +31,17 @@ m_name = gets.chomp
 puts "What's your last name?"
 l_name = gets.chomp
 puts "Chris Pine is cooler than you, #{f_name} #{m_name} #{l_name}."
+
+# ---- Bigger, better favorite number ----
+# Ask for a persons fav. number.
+# Have the program add 1 to the number
+# suggest the result as a bigger and better fav. number. (??)
+
+# How I did it
+puts 'What is your favorite number?'
+fnum = gets.chomp
+puts 'Your favorite number is ' + fnum
+puts 'but the bigger and better number is' + fnum + 1
+#no implicit conversion of Fixnum into String (TypeError)
+
+# How the book did it
