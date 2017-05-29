4e7379daaa4f855a24ccba070d2d62c2c10797c7
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-28 23:03:42 -0500

    mult. strings & more

--- pine/chapter-03/strings.rb
+++ pine/chapter-03/strings.rb
@@ -9,5 +9,11 @@ puts 'Good-bye.'
 # 3.1 String Arithmetic
 # adding strings
 puts 'I like' + 'apple pie.'
-#added space!!
+# added space!!
 puts 'I like' + ' apple pie.'
+
+# you can multiply strings too
+puts 'blink ' * 4
+# blink blink blink blink
+
+# numbers vs digits
