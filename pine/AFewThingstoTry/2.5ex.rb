c7f504fb28fd6699113d340a186bdaad80089c50
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-28 22:35:44 -0500

    Completed years exercise, added prompt for the rest of the exercises.

---
+++ pine/AFewThingstoTry/2.5ex.rb
@@ -0,0 +1,22 @@
+# A Few Things to Try...
+# 2.5-Exercise
+
+# Hours in a year
+puts "How many hours are in a year?"
+puts "Hours in a year: #{24 * 365} "
+
+#Leap Year?
+puts "Hours in a leap year: #{24 * 366}"
+
+# Minutes in a decade
+puts "How many minutes are in a decade?"
+puts "Minutes in a decade: "
+
+
+# Age in seconds, 23
+puts "How many seconds old are you?"
+puts "I'm #{} seconds old."
+
+#Authors Age
+puts "If I am 1,025 million seconds old -- how old am I?"
+puts "You are #{} years old"
