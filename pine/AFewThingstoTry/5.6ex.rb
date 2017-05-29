d4841da584adb835a7c84524876c20c2e1a6c20b
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-29 11:17:10 -0500

    added book example

--- pine/AFewThingstoTry/5.6ex.rb
+++ pine/AFewThingstoTry/5.6ex.rb
@@ -2,11 +2,22 @@
 # 5.6-Exercise page 28
 
 # Ask for full name, and greet user
-# How I did it
-puts 'Hello. What is your first name?'
-fname = gets.chomp
+## How I did it
+#puts 'Hello. What is your first name?'
+#fname = gets.chomp
+#puts 'What is your middle name?'
+#mname = gets.chomp
+#puts 'What is your last name?'
+#lname = gets.chomp
+#puts 'Pleased to meet you, ' + fname + ' ' + mname + ' ' +lname + '. :D'
+
+# Other ways to do it (book)
+puts 'What is your first name?'
+f_name = gets.chomp
 puts 'What is your middle name?'
-mname = gets.chomp
+m_name = gets.chomp
 puts 'What is your last name?'
-lname = gets.chomp
-puts 'Pleased to meet you, ' + fname + ' ' + mname + ' ' +lname + '. :D'
+l_name = gets.chomp
+
+full_name = f_name + ' ' + m_name + ' ' + l_name
+puts 'Hello, ' + full_name + '!'
