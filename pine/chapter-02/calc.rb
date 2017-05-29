7126be446dfb3d4cc0268c08c1ff69f9907beb92
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-28 21:53:47 -0500

    comments and stuff

--- pine/chapter-02/calc.rb
+++ pine/chapter-02/calc.rb
@@ -5,7 +5,15 @@
 puts 1 + 2
 
 # 2.4 Simple Arithmetic
+# floats
 puts 1.0 + 2.0
 puts 2.0 * 3.0
 puts 5.0 - 8.0
 puts 9.0 / 2.0
+
+# integers
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+# 'puts 9 / 2' will output '4' --  rounding down from 4.5 answer
