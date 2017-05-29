429528451947ba176b26e00bc98fce868f36b3be
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-28 23:10:41 -0500

    Understanding the difference between stirngs and numbers

--- pine/chapter-03/strings.rb
+++ pine/chapter-03/strings.rb
@@ -14,9 +14,27 @@ puts 'I like' + ' apple pie.'
 
 # you can multiply strings too
 puts 'blink ' * 4
-# blink blink blink blink
+#blink blink blink blink
 
-# numbers vs digits
+# ---- NUMBERS VS DIGITS ----
 # 12 is a number but '12' is a string of two digits
-
 puts 12 + 12
+#24
+
+puts '12' + '12'
+#1212
+
+puts '12 + 12'
+#'12 +12'
+
+# ---- How about this?.. ----
+puts 2 * 5
+#10
+
+puts '2' * 5
+#22222
+
+puts '2 * 5'
+#'2 * 5'
+
+# 3.3 Problems
