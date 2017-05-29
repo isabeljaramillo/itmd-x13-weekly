02b580001a70a94b6de13674c911f79b9747da86
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-29 11:11:40 -0500

    First part of exercise.

---
+++ pine/AFewThingstoTry/5.6ex.rb
@@ -0,0 +1,12 @@
+# A Few Things to Try...
+# 5.6-Exercise page 28
+
+# Ask for full name, and greet user
+# How I did it
+puts 'Hello. What is your first name?'
+fname = gets.chomp
+puts 'What is your middle name?'
+mname = gets.chomp
+puts 'What is your last name?'
+lname = gets.chomp
+puts 'Pleased to meet you, ' + fname + mname + lname + '. :D'
