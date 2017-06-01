faaaf1c67188cbe1a3318b968a320fea2e699774
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-31 19:36:50 -0500

    added separete notes for variables

---
+++ pine/chapter-05/variables.rb
@@ -0,0 +1,16 @@
+# variables.rb
+# Chapter 5 Mixing it Up - starts on page 23
+
+# 5.7 Mind your Variables
+# know where they are pointing to
+
+## Running this program will return an error like;
+# my_bith_month = 'August'
+# my_birth_day = 3
+
+# puts my_birth_month + my_birth_day
+
+#TypeError: can't convert Fixnum into String.
+
+# Fixnim is an INTEGER - smaller numbers
+# Bignum real big numbers
