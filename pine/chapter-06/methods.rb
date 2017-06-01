99abc493d570200a1eb44f0063fd9c30213c2975
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-05-31 20:13:42 -0500

    Lists examples of methods

---
+++ pine/chapter-06/methods.rb
@@ -0,0 +1,16 @@
+# methods.rb
+# Chapter 6 More About Methods - starts on page 30
+
+# List 10 methods
+# puts
+# gets
+# chomp
+# to_i
+# to_f
+# to_s
+# +
+# -
+# *
+# /
+
+# Methods are things that do stuff
