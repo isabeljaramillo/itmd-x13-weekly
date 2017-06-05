d5a954aacbaaf0e125b71bd41ec10a5c741a8eab
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 16:54:05 -0500

    Instructions for Deaf Grandma

---
+++ pine/AFewThingstoTry/7.5.1ex.rb
@@ -0,0 +1,9 @@
+# A Few Things to Try...
+# 7.5.1-Exercise page 57
+
+# Deaf Grandma
+# Write a program over a conversation with grandma, she should respond shouting
+# and asking you to speak up if she cannot hear you (lowercase). She should
+# respond if you write your text in uppercase letter (like your shouting)
+
+# How I did it --  if I understood this correctly,..
