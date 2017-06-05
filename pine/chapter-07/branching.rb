c05eebceaca607d3feed0a04174b3010b175442a
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 15:29:41 -0500

    else

--- pine/chapter-07/branching.rb
+++ pine/chapter-07/branching.rb
@@ -8,5 +8,17 @@ puts 'Hello, ' + name + '.'
 if name == 'Chris'
   puts 'What a lovely name!'
 end
-
 # if what comes after IF is true, we run the code between if and end.
+
+# Use ELSE when you want the program to do one thing if true, and something
+# ELSE when false.
+
+puts 'I am a fortune-teller. Tell me your name:'
+name = gets.chomp
+
+if name == 'Chris'
+  puts 'I see great things in your future.'
+else
+  puts 'Your future is.. oh my! Look at the time!'
+  puts 'I really have to go, sorry!'
+end
