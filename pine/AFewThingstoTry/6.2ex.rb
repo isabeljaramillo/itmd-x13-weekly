1da8ad381ace0f49dcaab23f9ec60957ef68deac
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 13:17:46 -0500

    How Chris would do 'Angry Boss' program

--- pine/AFewThingstoTry/6.2ex.rb
+++ pine/AFewThingstoTry/6.2ex.rb
@@ -11,3 +11,13 @@
 puts 'What do you want?!'
 want = gets.chomp
 puts 'WHAT?! "' + want.upcase + '"?! YOU\'RE FIRED!!'
+
+# How Chris would do it
+names = ['johnson', 'smith', 'weinberg', 'filmore']
+puts "CAN'T YOU SEE I'M BUSY?! MAKE IT FAST, #{names[rand(4)].upcase}!"
+request = gets.chomp
+puts "WHADDAYA MEAN \"#{request.upcase}\"?!? YOU'RE FIRED!!"
+#output:
+# CAN' T YOU SEE I' M BUSY?! MAKE IT FAST, WEINBERG!
+# I quit
+# WHADDAYA MEAN "I QUIT"?!? YOU'RE FIRED!!
