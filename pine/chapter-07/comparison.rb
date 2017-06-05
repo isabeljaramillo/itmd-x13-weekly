f4b8b09c3568f7440b036398df5bdd6f10299f9b
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 14:32:09 -0500

    Comparison methods with numbers

---
+++ pine/chapter-07/comparison.rb
@@ -0,0 +1,17 @@
+# comparison.rb
+# Chapter 7 Flow Control - starts on page 41
+
+# In the past, we made programs that said different things
+# depending on your keyboard input, but after this chapter they will actually
+# do different things. But how will we determine when to do one thing instead
+# of another? We need...
+
+# 7.1 Comparison Methods
+puts 1 > 2
+puts 1 < 2
+
+puts 5 >= 5
+puts 5 <= 4
+
+puts 1 == 1
+puts 2 != 1
