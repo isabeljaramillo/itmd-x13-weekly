a60eff8d58c9ae0db2f7f1a0e82c7339584a7192
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 15:20:31 -0500

    First branching examples

---
+++ pine/chapter-07/branching.rb
@@ -0,0 +1,12 @@
+# branching.rb
+# Chapter 7.2 (continuted) Branching - starts on page 43
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris'
+  puts 'What a lovely name!'
+end
+
+# if what comes after IF is true, we run the code between if and end.
