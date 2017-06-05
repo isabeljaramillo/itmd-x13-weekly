82431bd03ab3acf6df2b2327870161934d94666f
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 14:36:03 -0500

    lexicographical ordering

--- pine/chapter-07/comparison.rb
+++ pine/chapter-07/comparison.rb
@@ -15,3 +15,16 @@ puts 5 <= 4
 
 puts 1 == 1
 puts 2 != 1
+
+# Comparison methods with lexicographical ordering, the order in which
+# they show up in the dictionary
+
+puts 'cat' < 'dog'
+
+# Watch for upcase/lowecase though..
+# The way computers usually do things, they order capital letters as
+# coming before lowercase letters.
+puts 'bug lady' < 'Xander'
+# false
+puts 'bug lady'.downcase < 'Xander'.downcase
+# true
