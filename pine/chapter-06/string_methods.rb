4049de034dd452e2cfeffa1ddec0f31a9b49fc3f
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 12:49:39 -0500

    Left/Right Justify Methods

--- pine/chapter-06/string_methods.rb
+++ pine/chapter-06/string_methods.rb
@@ -84,3 +84,18 @@ puts ('Eating her curds and whey,'.center(line_width))
 puts ('When along came a spider'.center(line_width))
 puts ('Who sat down beside her'.center(line_width))
 puts ('And scared ehr poor shoe dog away.'.center(line_width))
+
+
+# ---- Left Justify(ljust) and Right(rjust) Justify ----
+line_width = 40
+str = '--> text <--'
+puts(str.ljust( line_width))
+puts(str.center(line_width))
+puts(str.rjust( line_width))
+puts(str.ljust(line_width/2) + str.rjust(line_width/2))
+
+## output:
+#--> text <--
+#              --> text <--
+#                            --> text <--
+#--> text <--                --> text <--
