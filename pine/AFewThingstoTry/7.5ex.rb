4d7c4dba6ccfe768dba0ba4e67475d848b402a74
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 16:39:52 -0500

    How I would do '99 bottles of beer on the wall'

--- pine/AFewThingstoTry/7.5ex.rb
+++ pine/AFewThingstoTry/7.5ex.rb
@@ -5,3 +5,19 @@
 # Write a program that prints out the lyrics.
 
 # How I did it
+num_start = 99
+num_update = num_start
+
+while num_update > 2
+  puts num_update.to_s + ' bottles of beer ont he wall, '
+       num_update.to_s + 'bottles of beer!'
+
+  num_update = num_update - 1
+  puts 'Take one down, pass it around, ' +
+       num_update.to_s + ' bottles of beer on the wall!'
+end
+## Hard codes the last two
+puts "2 bottles of beer on the wall, 2 bottles of beer!"
+puts "Take one down, pass it around, 1 bottle of beer on the wall!"
+puts "1 bottle of beer on the wall, 1 bottle of beer!"
+puts "Take one down, pass it around, no more bottles of beer on the wall!"
