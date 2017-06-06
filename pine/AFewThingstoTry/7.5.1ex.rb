43a53b79da32c5d32dab7159f6473c67da1b37d9
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-06-05 19:21:14 -0500

    How Chris would complete Leap Year Program using 'next if' statements

--- pine/AFewThingstoTry/7.5.1ex.rb
+++ pine/AFewThingstoTry/7.5.1ex.rb
@@ -125,3 +125,17 @@ while year <= ends
 
   year += 1
 end
+
+# How Chris would do it
+puts 'Pick a starting year (like 1973 or something):'
+starting = gets.chomp.to_i
+puts 'Now pick an ending year:'
+ending = gets.chomp.to_i
+
+puts 'Check it out... these years are leap years:'
+
+(starting..ending).each do |year|
+  next if year%4 != 0
+  next if year%100 == 0 && year%400 != 0
+  puts year
+end
