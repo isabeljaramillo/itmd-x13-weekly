769ba411011ef471e0d72b5cddb0ff1343caacf6
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-07-03 00:38:56 -0500

    load data fixture

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures : products
+# ...
+end
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -48,6 +51,3 @@ class ProductTest < ActiveSupport::TestCase
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
   end
-
-
-end
