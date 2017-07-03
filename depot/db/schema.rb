dd09642432093bdfcd04682108d4fa4641fe1e9c
Isabel Jaramillo <ijaramillo@hawk.iit.edu>
2017-07-03 01:14:04 -0500

    Ran rails migration

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622155818) do
+ActiveRecord::Schema.define(version: 20170703061303) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
